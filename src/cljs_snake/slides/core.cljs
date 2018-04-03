d56224ddbaede421873d437556e25cf0edeaeafb
adz5a <antoine.d.zimmermann@gmail.com>
2018-04-03 16:45:48 +0200

    [slides] make the viewer generate a summary

--- src/cljs_snake/slides/core.cljs
+++ src/cljs_snake/slides/core.cljs
@@ -8,15 +8,6 @@
                          (dec)
                          (* -100)) "vh)"))
 
-(defn viewer
-  "this component is used a container for the presented slides"
-  [app-state & slides]
-
-  (apply vector :section {:className "overflow-hidden"
-                    :style {:transform (translation app-state)
-                            :transition "transform 0.8s"}}
-          slides))
-
 (defn slide
   "this component is a slide, it takes destructered
   keyword arguments"
@@ -31,19 +22,17 @@
 (defn viewer
   "this component is used a container for the presented slides"
   [app-state & slides]
+  ;; does a side effect on mounting
+  (swap! app-state assoc :slide-count (inc (count slides)))
+  (println "render here")
 
-  (apply vector :section {:className "overflow-hidden"
-                    :style {:transform (translation app-state)
-                            :transition "transform 0.8s"}}
-          slides))
-
-(defn slide
-  "this component is a slide, it takes destructered
-  keyword arguments"
-  [{:keys [title]
-    :or {title "no title"}}
-   & content]
-  (apply vector
-         :article {:className "pa3 pa5-ns vh-100 white"}
-         [:h1 {:className "f-headline-l"} title]
-         content))
+  ;; needs to have the same signatures, names can differ
+  ;; though
+  (fn [app-state & slides]
+    (apply vector :section {:className "overflow-hidden"
+                            :style {:transform (translation app-state)
+                                    :transition "transform 0.8s"}}
+           (conj slides [slide {:title "Summary"}
+                         [:ul (map (fn [[_ opts & __]]
+                                     [:li {:key (:title opts)} (:title opts)]) 
+                                   slides)]]))))
